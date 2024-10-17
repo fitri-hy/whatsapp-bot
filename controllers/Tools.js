f8e910770a441c73bb59eb59ff07b7b16e697fa1
fitri-hy <noreply.orzpartners@gmail.com>
2024-10-17 16:22:34 +0700

    update

--- controllers/Tools.js
+++ controllers/Tools.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // ========= Two Lines ===========
 // Advanced Encryption Standard (AES)
